2f72301ecf7d5ddc68d2b4fdee989ec8e856d5a9
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2022-10-05 01:02:57 +0900

    fix: サーバのシャットダウンを追加、またオブジェクトがちゃんとドロップされるように改良

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ use std::{
     thread,
 };
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// 新しいThreadPoolを生成する。
@@ -18,10 +18,8 @@ impl ThreadPool {
     /// # パニック
     ///
     /// sizeが0なら、`new`関数はパニックします。
-    pub fn new(mut size: usize) -> ThreadPool {
-        if size == 0 {
-            size = 1;
-        }
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
 
@@ -32,7 +30,10 @@ impl ThreadPool {
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -40,23 +41,44 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 }
 
-pub struct Worker {
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            println!("ワーカー{}を終了します", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-
-            println!("ワーカー{}はジョブを受け取りました。実行します", id);
-            job();
+            match receiver.lock().unwrap().recv() {
+                Ok(job) => {
+                    println!("ワーカー{id}はジョブを受け取りました。実行します");
+                    job();
+                }
+                Err(_) => {
+                    println!("ワーカー{id}は切断されました。シャットダウンします");
+                    break;
+                }
+            }
         });
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
