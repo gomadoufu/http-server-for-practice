b695789c8ec33c7359c013dff5beb83f54a7a1f4
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2022-10-05 00:06:22 +0900

    add: シングルスレッドだったサーバを、マルチスレッド化した

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,62 @@
-pub struct ThreadPool;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+    /// 新しいThreadPoolを生成する。
+    ///
+    /// sizeがプールのスレッド数です。
+    ///
+    /// # パニック
+    ///
+    /// sizeが0なら、`new`関数はパニックします。
+    pub fn new(mut size: usize) -> ThreadPool {
+        if size == 0 {
+            size = 1;
+        }
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+pub struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("ワーカー{}はジョブを受け取りました。実行します", id);
+            job();
+        });
+        Worker { id, thread }
     }
 }
